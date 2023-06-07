5261d20d6a8457cdb0756bcd1b65b391e0a65d48
Dharamveer <dharammveer945734@outlook.com>
2023-06-08 01:08:17 +0530

    Final Commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
 
-    let x = 1235
+    let x = 123
      
      
     window.addEventListener('scroll', () => {
