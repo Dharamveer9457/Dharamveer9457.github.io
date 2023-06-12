7af23e0fbb8421324205e0928700eec162f1338f
Dharamveer <dharammveer945734@outlook.com>
2023-06-12 21:19:16 +0530

    Final Commit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
     let header = document.querySelector('header');
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
-
-
      
      
     window.addEventListener('scroll', () => {
